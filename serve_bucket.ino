2ae90eb4e5b3f81da02b0c30269be609e88da3b5
Olanshile Jimoh <51200418+olanshilejay@users.noreply.github.com>
2020-07-25 11:50:10 +0100

    Create serve_bucket.ino

---
+++ serve_bucket.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>; //import the servo library.
+int led = 13;
+int trig = 11;
+int echo = 12;
+int sPin = 8;
+long duration;
+float ft, cm;
+
+Servo servoC; //create an object of the servo library
+void setup() {
+  Serial.begin (9600);
+  pinMode (led, OUTPUT);
+  pinMode (trig, OUTPUT);
+  pinMode (echo, INPUT);
+  servoC.attach (sPin);
+
+  servoC.write (0);
+  delayMicroseconds (10);
+}
+
+void loop() {
+  digitalWrite (trig, LOW); //clean sheet
+  delay (100);
+  digitalWrite (trig, HIGH);
+  delay (100);
+  digitalWrite (trig, LOW);
+
+  duration = pulseIn (echo, HIGH);
+
+  cm = (duration/2)/29.1;
+  ft = cm/30.48;
+  Serial.println (cm);
+  Serial.println (ft);
+  if (ft <= 0.30 ){
+      Serial.println ("Yah, Dispense soap or water!!!");
+      digitalWrite (led, HIGH);
+      servoC.write (180);
+      delay (5000);
+      servoC.write(0);
+      digitalWrite (led, LOW);
+    }
+    else{
+      Serial.println ("Not in range!!!");
+    }
+  delay (1000);
+
+}
