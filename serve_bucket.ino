70f8cab2b942a99cbbb615ed93ce7be0c43411e1
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-13 06:37:35 +0100

    Update serve_bucket.ino

--- serve_bucket.ino
+++ serve_bucket.ino
@@ -6,7 +6,7 @@ int sPin = 8;
 long duration;
 float ft, cm;
 
-Servo servoC; //create an object of the
+Servo servoC; //create an object of
 void setup() {
   Serial.begin (9600);
   pinMode (led, OUTPUT);
